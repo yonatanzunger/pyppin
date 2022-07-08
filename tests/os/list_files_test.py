8740b97e4812c489837566a29856a655d29eeb1e
Yonatan  Zunger <zunger@desiderata.lan>
2022-07-08 13:42:15 -0700

    Remove data inputs from list files test, so pip install works.

--- tests/os/list_files_test.py
+++ tests/os/list_files_test.py
@@ -1,11 +1,33 @@
+import os
 import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from pyppin.os.list_files import list_files
 
 
 class ListFilesTest(unittest.TestCase):
     def testListFiles(self) -> None:
+        # We're going to create our test environment here, rather than storing it as data in the git
+        # repository. This is because we deliberately want to create weird things like infinite-loop
+        # symlinks, which make git very unhappy.
+        with TemporaryDirectory() as root:
+            with open(f'{root}/foo.txt', 'w') as foo:
+                foo.write('foo\n')
+            os.symlink(f'{root}/foo.txt', f'{root}/bar.txt')
+            os.mkdir(f'{root}/skipme')
+            with open(f'{root}/skipme/not_seen.txt', 'w') as not_seen:
+                not_seen.write('skipme\n')
+            os.mkdir(f'{root}/subdir')
+            with open(f'{root}/subdir/quux.txt', 'w') as quux:
+                quux.write('quux\n')
+            # Yee-haw!
+            os.symlink('..', f'{root}/subdir/loop')
+
+        # OK, now let's see if we can list this mess correctly. Note that the only "real" files are
+        # the ones we generated with calls to write() above; everything else is a symlink or
+        # directory that should not show up in the output.
+
         # Set up an absolute path to make it as hard as possible for relative_to to work below.
         root = Path(__file__).parent.joinpath("list_files_test_data").resolve()
         found = list_files(root, select=lambda path: "skip" not in path.name)
