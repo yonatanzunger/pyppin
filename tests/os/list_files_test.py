d76e943aefe812af977f534586ffeb82b85d2e5e
Yonatan  Zunger <zunger@gmail.com>
2022-07-18 13:30:04 -0700

    Remove rate limiter for now

--- tests/os/list_files_test.py
+++ tests/os/list_files_test.py
@@ -1,6 +1,5 @@
 import os
 import unittest
-from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from pyppin.os.list_files import list_files
@@ -24,22 +23,23 @@ class ListFilesTest(unittest.TestCase):
             # Yee-haw!
             os.symlink('..', f'{root}/subdir/loop')
 
-        # OK, now let's see if we can list this mess correctly. Note that the only "real" files are
-        # the ones we generated with calls to write() above; everything else is a symlink or
-        # directory that should not show up in the output.
+            # OK, now let's see if we can list this mess correctly. Note that the only "real" files
+            # are the ones we generated with calls to write() above; everything else is a symlink or
+            # directory that should not show up in the output.
 
-        # Set up an absolute path to make it as hard as possible for relative_to to work below.
-        root = Path(__file__).parent.joinpath("list_files_test_data").resolve()
-        found = list_files(root, select=lambda path: "skip" not in path.name)
+            # Set up an absolute path to make it as hard as possible for relative_to to work below.
+            # root = Path(__file__).parent.joinpath("list_files_test_data").resolve()
+            found = list_files(root, select=lambda path: "skip" not in path.name)
 
-        # The fact that relative_to will work in this case is a guaranteed invariant of list_files!
-        relative = [str(path.relative_to(root)) for path in found]
+            # The fact that relative_to will work in this case is a guaranteed invariant of
+            # list_files!
+            relative = [str(path.relative_to(root)) for path in found]
 
-        # Things that do *not* show up in this output:
-        #   - bar.txt is a symlink to foo.txt, so we don't yield it twice.
-        #   - subdir/loop is an infinite-loop symlink, and we don't fall into that trap.
-        #   - skipme/not_seen.txt is skipped because of the select function.
-        self.assertEqual(["foo.txt", "subdir/quux.txt"], sorted(relative))
+            # Things that do *not* show up in this output:
+            #   - bar.txt is a symlink to foo.txt, so we don't yield it twice.
+            #   - subdir/loop is an infinite-loop symlink, and we don't fall into that trap.
+            #   - skipme/not_seen.txt is skipped because of the select function.
+            self.assertEqual(["foo.txt", "subdir/quux.txt"], sorted(relative))
 
 
 if __name__ == "__main__":
