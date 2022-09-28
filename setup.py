9fe670dfe3e476750384548798f8298cacb0e803
Yonatan  Zunger <zunger@gmail.com>
2022-09-28 12:10:16 -0700

    Bump version to 1.0.1 to make PyPi happy

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(
 
 setup(
     name="pyppin",
-    version="1.0.0",
+    version="1.0.1",
     description="Python tools collection",
     long_description=long_description,
     long_description_content_type="text/markdown",
